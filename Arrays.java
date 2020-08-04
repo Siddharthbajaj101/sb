d0ffea89038d86417491532e8ec2e418171383b0
Siddharthbajaj101 <63838975+Siddharthbajaj101@users.noreply.github.com>
2020-08-04 18:16:19 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,14 @@
+
+
+public class Arrays
+{
+    int a[] = {12,3,54};
+    public void display()
+     {
+         for(int i = 0;i<=2;i++)
+         {
+             System.out.println(a[i]);
+         }
+     }
+     
+}
